6b5179a411d2035aa4cbdd8414cce0ebf39085a2
“[Gyanaram <[gyanaramnitian@gmail.com]>
2024-10-08 23:28:50 +0530

    bhai yr ma ja

--- src/components/Homecomponent.jsx
+++ src/components/Homecomponent.jsx
@@ -1,6 +1,6 @@
 // import React from 'react'
 // import Card
-import Cardcomponent from '../components/Cardcomponent'
+// import Cardcomponent from '../components/Cardcomponent'
 export default function Homecomponent() {
   return (
      <>
