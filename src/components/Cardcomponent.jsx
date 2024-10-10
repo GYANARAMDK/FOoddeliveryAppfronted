dff7f57394b39bec29fdfbb63f6065e35165da7d
“[Gyanaram <[gyanaramnitian@gmail.com]>
2024-10-10 11:00:44 +0530

    new files

--- src/components/Cardcomponent.jsx
+++ src/components/Cardcomponent.jsx
@@ -1,110 +1,38 @@
 // import React from 'react'
 
-export default function cardcomponent() {
+export default function cardcomponent({ product }) {
   return (
     <>
+      <div>
+        <h1>Product List</h1>
+        <ul className="product-grid">
+          {product.map((product) => (
+            <li key={product._id} className="product-item">
+              <h2>{product.name}</h2>
+              <p>Price: ${product.price}</p>
+              <p>{product.description}</p>
+              <img src={product.imageUrl} alt={product.name} />
+              <div>
+                <button className="bg-slate-500 p-2  rounded">
+                  add to cart
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+
       {/* <div className="  flex ">
         <div className=" bg-blue-500 m-3 p-2 flex flex-col w-[20vw] ">
-          <div className="w-[100vw]">
-            <img src="another.png" alt="" />
+          <div >
+            <img src="another.png" alt="" className="size-cover" />
           </div>
           <div className="flex justify-evenly items-center mt-2  ">
             <div className=" bg-gray-400 ">
-              <p className="px-2 ">  sharp counmean    </p>
+              <p className="px-2  ">  sharp counmean    </p>
             </div>
             <div>
-              <button className="bg-slate-500 p-2 rounded">add to cart</button>
-            </div>
-          </div>
-        </div>
-        <div className=" bg-blue-500 m-3 p-2 flex flex-col w-[20vw] ">
-          <div className="w-[100vw]">
-            <img src="another.png" alt="" />
-          </div>
-          <div className="flex justify-evenly items-center mt-2  ">
-            <div className=" bg-gray-400 ">
-              <p className="px-2 ">  sharp counmean    </p>
-            </div>
-            <div>
-              <button className="bg-slate-500 p-2 rounded">add to cart</button>
-            </div>
-          </div>
-        </div>
-        <div className=" bg-blue-500 m-3 p-2 flex flex-col w-[20vw] ">
-          <div className="w-[100vw]">
-            <img src="another.png" alt="" />
-          </div>
-          <div className="flex justify-evenly items-center mt-2  ">
-            <div className=" bg-gray-400 ">
-              <p className="px-2 ">  sharp counmean    </p>
-            </div>
-            <div>
-              <button className="bg-slate-500 p-2 rounded">add to cart</button>
-            </div>
-          </div>
-        </div>
-        <div className=" bg-blue-500 m-3 p-2 flex flex-col w-[20vw] ">
-          <div className="w-[100vw]">
-            <img src="another.png" alt="" />
-          </div>
-          <div className="flex justify-evenly items-center mt-2  ">
-            <div className=" bg-gray-400 ">
-              <p className="px-2 ">  sharp counmean    </p>
-            </div>
-            <div>
-              <button className="bg-slate-500 p-2 rounded">add to cart</button>
-            </div>
-          </div>
-        </div>
-        <div className=" bg-blue-500 m-3 p-2 flex flex-col w-[20vw] ">
-          <div className="w-[100vw]">
-            <img src="another.png" alt="" />
-          </div>
-          <div className="flex justify-evenly items-center mt-2  ">
-            <div className=" bg-gray-400 ">
-              <p className="px-2 ">  sharp counmean    </p>
-            </div>
-            <div>
-              <button className="bg-slate-500 p-2 rounded">add to cart</button>
-            </div>
-          </div>
-        </div>
-        <div className=" bg-blue-500 m-3 p-2 flex flex-col w-[20vw] ">
-          <div className="w-[100vw]">
-            <img src="another.png" alt="" />
-          </div>
-          <div className="flex justify-evenly items-center mt-2  ">
-            <div className=" bg-gray-400 ">
-              <p className="px-2 ">  sharp counmean    </p>
-            </div>
-            <div>
-              <button className="bg-slate-500 p-2 rounded">add to cart</button>
-            </div>
-          </div>
-        </div>
-        <div className=" bg-blue-500 m-3 p-2 flex flex-col w-[20vw] ">
-          <div className="w-[100vw]">
-            <img src="another.png" alt="" />
-          </div>
-          <div className="flex justify-evenly items-center mt-2  ">
-            <div className=" bg-gray-400 ">
-              <p className="px-2 ">  sharp counmean    </p>
-            </div>
-            <div>
-              <button className="bg-slate-500 p-2 rounded">add to cart</button>
-            </div>
-          </div>
-        </div>
-        <div className=" bg-blue-500 m-3 p-2 flex flex-col w-[20vw] ">
-          <div className="w-[100vw]">
-            <img src="another.png" alt="" />
-          </div>
-          <div className="flex justify-evenly items-center mt-2  ">
-            <div className=" bg-gray-400 ">
-              <p className="px-2 ">  sharp counmean    </p>
-            </div>
-            <div>
-              <button className="bg-slate-500 p-2 rounded">add to cart</button>
+              <button className="bg-slate-500 p-2  rounded">add to cart</button>
             </div>
           </div>
         </div>
--- src/components/Homecomponent.jsx
+++ src/components/Homecomponent.jsx
@@ -1,11 +1,36 @@
 // import React from 'react'
 // import Card
-// import Cardcomponent from '../components/Cardcomponent'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Cardcomponent from "../components/Cardcomponent";
+import './/Homecomponent.css'
 export default function Homecomponent() {
+  const [product, setproduct] = useState([]);
+  const [loaing, setloading] = useState(true);
+  const [error, seterror] = useState(null);
+  useEffect(() => {
+    const fetchproducts = async () => {
+      try {
+        const response = await axios.get(
+          "https://webproject-1-707h.onrender.com/api/products"
+        );
+       
+        setproduct(response.data);
+       
+      } catch (error) {
+        seterror(error);
+        console.log(error);
+       
+      } finally {
+        setloading(false);
+      }
+    };
+    fetchproducts();
+  }, []);
   return (
-     <>
-     <Cardcomponent/>
-    
-     </>
-  )
+    <>
+      
+      <Cardcomponent product={product} />
+    </>
+  );
 }
